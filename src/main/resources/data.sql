717254338d1c7f2761f27eb1d5e8bf247ba44e56
doramonz <whs3867@gmail.com>
2024-05-17 15:57:03 +0900

    [Feature] : DB

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -233,11 +233,24 @@ create table goals
     unit_price integer
 );
 
-insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-01-01', 2000, 1800, 8600);
-insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-02-01', 2300, 1500, 2400);
-insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-03-01', 1800, 2000, 12200);
-insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-04-01', 2500, 2500, 11000);
-insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-05-01', 1900, 0, 10600);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-01-01', 6000, 4800, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-02-01', 5000, 6400, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-03-01', 7000, 6600, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-04-01', 6000, 5300, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-05-01', 5000, 5500, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-06-01', 6000, 4800, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-07-01', 5000, 6400, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-08-01', 7000, 6600, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-09-01', 6000, 5300, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-10-01', 5000, 5500, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-11-01', 5000, 4500, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2023-12-01', 5000, 5500, 1500);
+
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-01-01', 6000, 4800, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-02-01', 5000, 6400, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-03-01', 7000, 6600, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-04-01', 6000, 5300, 1500);
+insert into goals(goal_date, goal_amount, amount, unit_price) values ('2024-05-01', 3000, null, 1500);
 
 create table control_sensors
 (
