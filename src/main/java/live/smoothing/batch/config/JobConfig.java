269b0a91c7df3c3d3562c3ad5a2190b79f2bfe63
doramonz <whs3867@gmail.com>
2024-05-23 14:13:54 +0900

    [Feature] : Schedule 적용

--- src/main/java/live/smoothing/batch/config/JobConfig.java
+++ src/main/java/live/smoothing/batch/config/JobConfig.java
@@ -3,12 +3,16 @@ package live.smoothing.batch.config;
 import live.smoothing.batch.step.CheckThisMonthsGoalPassStep;
 import live.smoothing.batch.step.UserPointStep;
 import lombok.RequiredArgsConstructor;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 @EnableBatchProcessing
@@ -20,6 +24,7 @@ public class JobConfig {
     private final CheckThisMonthsGoalPassStep checkReachingThisMonthsTargetStep;
     private final UserPointStep userPointStep;
     private final Step sendReportEmail;
+    private final JobLauncher jobLauncher;
 
     @Bean("generateExcelReportAndSendEmailJob")
     public Job generateExcelReportAndSendEmailJob() {
@@ -35,6 +40,22 @@ public class JobConfig {
                 .start(checkReachingThisMonthsTargetStep.getThisMonthsGoalStep())
                 .next(userPointStep.getUsersStep())
                 .build();
+    }
+
+    @Scheduled(cron = "* * 1 1 * ?")
+    public void runGenerateExcelReportAndSendEmailJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameter = new JobParametersBuilder()
+                .addString("generateExcelReportAndSendEmailJob", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+        jobLauncher.run(generateExcelReportAndSendEmailJob(), jobParameter);
+    }
+
+    @Scheduled(cron = "* * 2 1 * ?")
+    public void runCheckReachingThisMonthsTargetJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        JobParameters jobParameter = new JobParametersBuilder()
+                .addString("checkReachingThisMonthsTargetJob", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+        jobLauncher.run(checkReachingThisMonthsTargetJob(), jobParameter);
     }
 
 }
